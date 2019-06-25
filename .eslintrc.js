6fec88cb5677ad6ab3de00f7901fffbe092d0297
yangbaize <ybz117000@163.com>
2019-06-25 11:47:40 +0800

    根据与后端接口的调用得到的数据-与极验服务器交互-显示弹窗 & 得到短信接口的数据

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,8 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'spaced-comment': ['error', 'always']
   },
   parserOptions: {
     parser: 'babel-eslint'
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,24 +3,44 @@
     <!-- 给组件加 class，会把这个 class 作用到组件的根元素上 -->
     <div class="form-wrap">
       <div class="form-head">
-        <img src="./logo_index.png" alt="黑马头条号">
+        <img
+          src="./logo_index.png"
+          alt="黑马头条号"
+        >
       </div>
-      <el-form class="form-content" ref="form" :model="form">
+      <el-form
+        class="form-content"
+        ref="form"
+        :model="form"
+      >
         <el-form-item>
-          <el-input v-model="form.mobile" placeholder="手机号"></el-input>
+          <el-input
+            v-model="form.mobile"
+            placeholder="手机号"
+          ></el-input>
         </el-form-item>
         <el-form-item>
           <!-- el-col 栅格布局，一共 24 列，:span 用来指定占用的大小，:offset 用来指定偏移量 -->
           <el-col :span="14">
-            <el-input v-model="form.code" placeholder="验证码"></el-input>
+            <el-input
+              v-model="form.code"
+              placeholder="验证码"
+            ></el-input>
           </el-col>
 
-          <el-col :offset="1" :span="9">
-            <el-button>获取验证码</el-button>
+          <el-col
+            :offset="1"
+            :span="9"
+          >
+            <el-button @click="handleSendCode">获取验证码</el-button>
           </el-col>
         </el-form-item>
         <el-form-item>
-          <el-button class="btn-login" type="primary" @click="onSubmit">登录</el-button>
+          <el-button
+            class="btn-login"
+            type="primary"
+            @click="onSubmit"
+          >登录</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -28,6 +48,9 @@
 </template>
 
 <script>
+import axios from 'axios'
+import '@/vendor/gt.js'
+
 export default {
   name: 'AppLogin',
   data () {
@@ -41,6 +64,34 @@ export default {
   methods: {
     onSubmit () {
       console.log('submit!')
+    },
+    // 与后端第一次交互
+    handleSendCode () {
+      const { mobile } = this.form
+      axios({
+        methods: 'GET',
+        url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${mobile}`
+      }).then(res => {
+        // 根据第一次与后端交互得到的数据-用于与极验服务器交互 -得到人机交互弹框-并得到申请短信接口的数据
+        const { data } = res.data
+        window.initGeetest({// 以下配置参数来自服务端 SDK
+          gt: data.gt,
+          challenge: data.challenge,
+          offline: !data.success,
+          new_captcha: data.new_captcha,
+          product: 'bind'
+        }, function (captchaObj) {
+          // 这里可以调用验证实例 captchaObj 的实例方法
+          captchaObj.onReady(function () {
+            // 验证码ready之后才能调用verify方法显示验证码
+            captchaObj.verify()
+          }).onSuccess(function () {
+            // your code
+          }).onError(function () {
+            // your code
+          })
+        })
+      })
     }
   }
 }
@@ -49,7 +100,7 @@ export default {
 <style lang="less" scoped>
 .login-wrap {
   height: 100%;
-  background-image: url('./login_bg.jpg');
+  background-image: url("./login_bg.jpg");
   display: flex;
   justify-content: center;
   align-items: center;

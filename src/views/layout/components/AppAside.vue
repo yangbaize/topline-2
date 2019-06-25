66e77b1040d03ef1d88c3a917511b0d9a1645b78
yangbaize <ybz117000@163.com>
2019-06-25 20:09:38 +0800

    完成侧边栏NavMenu布局

--- src/views/layout/components/AppAside.vue
+++ src/views/layout/components/AppAside.vue
@@ -1,7 +1,63 @@
 <template>
-  <div>
-    <p>AppAside</p>
-  </div>
+  <el-menu
+    default-active="2"
+    @open="handleOpen"
+    @close="handleClose"
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b"
+    :router="true"
+  >
+    <el-menu-item index="/">
+      <i class="el-icon-menu"></i>
+      <span slot="title">首页</span>
+    </el-menu-item>
+
+    <el-submenu index="1">
+      <template slot="title">
+        <i class="el-icon-location"></i>
+        <span>内容管理</span>
+      </template>
+      <el-menu-item index="1-1">
+        <i class="el-icon-menu"></i>
+        <span slot="title">发布文章</span>
+      </el-menu-item>
+      <el-menu-item index="1-2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">内容列表</span>
+      </el-menu-item>
+      <el-menu-item index="1-3">
+        <i class="el-icon-menu"></i>
+        <span slot="title">评论管理</span>
+      </el-menu-item>
+      <el-menu-item index="1-4">
+        <i class="el-icon-menu"></i>
+        <span slot="title">素材管理</span>
+      </el-menu-item>
+    </el-submenu>
+    <el-submenu index="2">
+      <template slot="title">
+        <i class="el-icon-location"></i>
+        <span>粉丝管理</span>
+      </template>
+      <el-menu-item index="2-1">
+        <i class="el-icon-menu"></i>
+        <span slot="title">图文数据</span>
+      </el-menu-item>
+      <el-menu-item index="2-2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">粉丝概况</span>
+      </el-menu-item>
+      <el-menu-item index="2-3">
+        <i class="el-icon-menu"></i>
+        <span slot="title">粉丝列表</span>
+      </el-menu-item>
+    </el-submenu>
+    <el-menu-item index="3">
+      <i class="el-icon-menu"></i>
+      <span slot="title">账户管理</span>
+    </el-menu-item>
+  </el-menu>
 </template>
 
 <script>
@@ -9,6 +65,14 @@ export default {
   name: 'AppAside',
   data () {
     return {}
+  },
+  methods: {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
+    }
   }
 }
 </script>
